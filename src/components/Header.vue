fda8a04d927c1d52d22b7b53c90e1e198569f271
francosbenitez <francosbenitez@gmail.com>
2021-10-24 09:10:52 -0300

    feat: added font-awesome

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="text-center p-4 bg-primary text-white">
+    <div class="fs-3 font-weight-bold mb-3">
+      <i class="fa fa-viruses"></i>
+      Covid Tracker App
+    </div>
+    <p>Api by <a class="text-white" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+  // name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
